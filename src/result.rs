c0738d675d0c3b626ff3bd299e3dd5e9e83ffb55
Vinoth <vinothnayagar@gmail.com>
2023-03-04 23:42:06 +0530

    Result<T, String>

--- src/result.rs
+++ src/result.rs
@@ -78,3 +78,54 @@ fn manin() {
     let purchased = try_purchase(&vino);
     println!("{:?}", purchased);
 }
+
+enum Position {
+    Maintenance,
+    Marketing,
+    Manager,
+    LineSupervisor,
+    KitchenStaff,
+    AssemblyTech,
+}
+
+enum Status {
+    Active,
+    Terminated,
+}
+
+struct Employee {
+    position: Position,
+    status: Status,
+}
+
+fn try_access(emp: &Employee) -> Result<(), String> {
+    match employee.status {
+        Status::Terminated => return Err("Terminated".to_owned()),
+        _ => (),
+    };
+
+    match employee.position {
+        Position::Maintenance => Ok(()),
+        Position::Marketing => Ok(()),
+        Position::Manager => Ok(()),
+        _ => Err("Invalid Position".to_owned()),
+    }
+}
+
+fn print_access(emp: &Employee) -> Result<(), String> {
+    let attempt_access = try_access(emp)?;
+    println!("access ok");
+    Ok(())
+}
+
+fn main() {
+    let manager = Employee {
+        position: Position::Manger,
+        status: Status::Active,
+    };
+
+    match print_access(&manager) {
+        Err(e) => println!("access denied: {:?}"),
+        _ => (),
+    }
+}
