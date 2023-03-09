6ed2717aa58a22b4400cc00c0ef440cb71495eb7
Vinoth <vinothnayagar@gmail.com>
2023-03-09 20:24:22 +0530

    console problems practice

--- basic-tasks/src/main.rs
+++ basic-tasks/src/main.rs
@@ -1,3 +1,4 @@
+use chrono::prelude::*;
 use std::io;
 
 // Simple task in Rust that involves taking user input and printing it back to the console:
@@ -29,7 +30,44 @@ fn ms() {
     println!("the sum of you entered numbers is  : {:?}", c);
 }
 
+// Write a program that reads  integers, one integer per line.
+// The program should output the result to the console.
+fn task2() {
+    let mut input = String::new();
+
+    for i in 1..5 {
+        println!("Please Enter numbers");
+        let mut tempStr = String::new();
+        io::stdin().read_line(&mut tempStr).expect("Invalid number");
+        input.push_str(&tempStr.trim());
+    }
+    println!("{:?}", input);
+}
+
+//Write a program that asks the user for their name and age, and then outputs a message with their name and the year they will turn 100 years old.
+//For example, if the user enters "Alice" and "25", the program should output something like: "Hello, Alice! You will turn 100 in the year 2096."
+fn task3() {
+    println!("Started...");
+
+    let mut name = String::new();
+    let mut age = String::new();
+    println!("What is your name:");
+    io::stdin().read_line(&mut name).expect("Enter valid");
+    println!("What is your age:");
+    io::stdin().read_line(&mut age).expect("Enter valid number");
+
+    let a1: i32 = age.trim().parse().expect("Not a vaild number");
+
+    // Calculating the year the user will turn 100
+    let currentYear = Utc::now().year() as i32;
+    let birthYear = (currentYear - a1);
+    let yr_of_100th = birthYear + 100;
+    print!("{:?}", yr_of_100th);
+}
+
 fn main() {
     // task1();
-    ms();
+    // ms();
+    // task2();
+    task3();
 }
