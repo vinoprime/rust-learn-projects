d06ea580a359f6958f18587465df16b268884572
Vinoth <vinothnayagar@gmail.com>
2023-09-24 12:43:30 +0530

    added grpc

---
+++ mailer-api/src/main.rs
@@ -0,0 +1,16 @@
+
+
+// use mongodb::bson::{doc, Document};
+
+// pub trait Repository {
+//     fn create(&self, document: Document) -> Result<(), mongodb::error::Error>;
+//     fn read(&self, id: &str) -> Result<Document, mongodb::error::Error>;
+//     fn update(&self, id: &str, document: Document) -> Result<(), mongodb::error::Error>;
+//     fn delete(&self, id: &str) -> Result<(), mongodb::error::Error>;
+// }
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
