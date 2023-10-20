1c44c60d25ff38b14cb181d883e6f932b668729f
Vinoth <vinothnayagar@gmail.com>
2023-10-20 23:18:36 +0530

    actix-web http call added

--- http-client/src/main.rs
+++ http-client/src/main.rs
@@ -8,8 +8,11 @@ use actix_web::{
     Responder,
     middleware::Logger,
 };
-
 use std::fmt;
+use chrono::{ Duration };
+use reqwest;
+
+mod Auth;
 
 #[derive(Debug)]
 enum CustomError {
@@ -50,6 +53,27 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+    Auth::my_function();
+
+    let two_hours = Duration::hours(2);
+    let secret_key = b"secret";
+    let response = Auth::generate_access_token(secret_key, "1", two_hours);
+    match response {
+        Ok(token) => {
+            // token is the extracted string from the Ok variant
+            println!("Token received: {}", token);
+            let decoded = Auth::decode_token::<()>(&token, secret_key);
+            match decoded {
+                Ok(d) => println!("{:?}", d),
+                Err(er) => println!("{:?}", er),
+            }
+            // println!("Hello, world! {:?}", decoded);
+        }
+        Err(err) => {
+            // handle the error
+            println!("Error: {}", err);
+        }
+    }
 
     let _ = HttpServer::new(|| { App::new().wrap(Logger::default()).service(greet).service(index) })
         .bind(("0.0.0.0", 8080))?
